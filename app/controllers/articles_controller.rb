a953e5d5ef04ba19e5d80b2cb695967561761944
Eddie Kao <eddie@5xcampus.com>
2023-07-14 17:00:51 +0800

    整理 CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
-    @articles = Article.all.order(id: :desc)
+    @articles = Article.order(id: :desc)
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,12 +23,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to articles_path, notice: '更新成功'
     else
@@ -36,7 +34,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path, notice: '刪除成功'
   end
@@ -45,5 +42,9 @@ class ArticlesController < ApplicationController
   # Strong Paramenter
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
