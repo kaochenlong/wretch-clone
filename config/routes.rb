ff3110ab9fb9192efcd9b4be7fd9b6588cb965b0
Eddie Kao <eddie@5xcampus.com>
2023-07-14 17:13:21 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  get '/articles', to: 'articles#index'
-  get '/articles/new', to: 'articles#new', as: 'new_article'
-  post '/articles', to: 'articles#create'
-  # 單一頁面
-  get '/articles/:id', to: 'articles#show', as: 'article'
-  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
-  patch '/articles/:id', to: 'articles#update'
-  delete '/articles/:id', to: 'articles#destroy'
+  resources :articles
 
   get '/about', to: 'pages#about'
 end
