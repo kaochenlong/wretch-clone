a14a44df189b2077fc9bd73757b8e52e87c01318
Eddie Kao <eddie@5xcampus.com>
2023-07-14 16:35:41 +0800

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all.order(id: :desc)
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
-    # 實體變數
     @article = Article.new
   end
 
@@ -11,12 +15,33 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to "/articles", notice: "文章新增成功"
+      redirect_to articles_path, notice: "文章新增成功"
     else
       render :new
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path, notice: '更新成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: '刪除成功'
+  end
+
+  private
   # Strong Paramenter
   def article_params
     params.require(:article).permit(:title, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  # get '/', to: 'articles#index'
   root 'articles#index'
 
   get '/articles', to: 'articles#index'
-  get '/articles/new', to: 'articles#new'
+  get '/articles/new', to: 'articles#new', as: 'new_article'
   post '/articles', to: 'articles#create'
+  # 單一頁面
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
 
   get '/about', to: 'pages#about'
 end
