a881945cfbced9cbe59fb6820e953fa6fc021b85
Eddie Kao <eddie@5xcampus.com>
2023-07-18 15:03:11 +0800

    add password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
+  validates :password, confirmation: true
+
   validates :email, presence: true,
                     uniqueness: true,
                     format: {
                       with: URI::MailTo::EMAIL_REGEXP,
                       message: '格式有誤'
                     }
+
+  before_create :encrypt_password
+
+  private
+  def encrypt_password
+    self.password = Digest::SHA1.hexdigest(self.password)
+  end
 end
